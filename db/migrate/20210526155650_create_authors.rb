442c445c82c6f355180266c16cbd023b49c00b92
Nkatha_Glory <kinyua6945@gmail.com>
2023-06-26 21:10:56 +0300

    Done

--- db/migrate/20210526155650_create_authors.rb
+++ db/migrate/20210526155650_create_authors.rb
@@ -2,6 +2,9 @@ class CreateAuthors < ActiveRecord::Migration[6.1]
   def change
     create_table :authors do |t|
       t.string :name
+      t.string :email
+      t.string :bio
+      t.string :avatar_url
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,9 @@ ActiveRecord::Schema.define(version: 2021_05_26_163430) do
 
   create_table "authors", force: :cascade do |t|
     t.string "name"
+    t.string "email"
+    t.string "bio"
+    t.string "avatar_url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
